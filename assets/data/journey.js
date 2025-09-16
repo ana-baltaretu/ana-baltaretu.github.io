a805f30f8be3f11dce4fb13540060e0614d3d3bd
Ana Baltaretu <ana.baltaretu@outlook.com>
2025-09-16 13:27:16 +0200

    Add content for HackDelft 2024

--- assets/data/journey.js
+++ assets/data/journey.js
@@ -1,5 +1,5 @@
 const journey = [
-    
+
     // {
     //     title: "First place at HackDelft 2024",
     //     imgSrc: "images/projects/folder/icon.png",
@@ -81,22 +81,23 @@ const journey = [
     //     resourceLink: null,
     //     year: "2022-2023"
     // },
-    // {
-    //     title: "Tetris robot",
-    //     imgSrc: "images/projects/tetris/tetris-robot.jpeg",
-    //     description: "Robotics minor, ROS, package detection (video + LIDAR)",
-    //     codeLink: "",
-    //     resourceLink: null,
-    //     year: "2021-2022"
-    // },
     {
-    title: "Tetris robot",
-    imgSrc: "images/projects/tetris/tetris-robot.jpeg",
-    description: "As part of the Robotics minor at TU Delft, I worked in a multidisciplinary team to design and build a 7-DOF robotic arm capable of stacking parcels into an airplane container. I focused on the software side, developing package detection using a camera and a <a href=\"https://en.wikipedia.org/wiki/Lidar\" target=\"_blank\">LiDAR</a>, and integrating these into <a href=\"https://www.ros.org/\" target=\"_blank\">ROS</a> for localization. I also contributed to testing and motion control, learning the basics of robotic middleware and real-time sensor fusion. The project was done in collaboration with <a href=\"https://www.vanderlande.com/\" target=\"_blank\">Vanderlande</a> and involved creating a full prototype robot from scratch, covering mechanics, electronics, and intelligent software.",
-    codeLink: "",
-    resourceLink: "assets/misc/Robotics_Handover_Document.pdf",
-    resourceLabel: "Report",
-    year: "2021-2022"
+        title: "First place at HackDelft 2024 (JetBrains Track)",
+        imgSrc: "https://upload.wikimedia.org/wikipedia/en/thumb/0/08/JetBrains_beam_logo.svg/2048px-JetBrains_beam_logo.svg.png",
+        description: "Led a team of 4 in developing an IDE extension to improve software documentation with AI assistance. I kept the team focused and motivated, ensuring we brainstormed effectively and converged on the best idea. Our solution targeted common documentation issues: outdated files, time wasted on onboarding, and difficulty navigating large codebases. We designed a plugin that generates up-to-date documentation, creates mind maps and visualizations for better understanding, and detects conceptual duplicates in code. Using a <a href=\"https://www.figma.com/\" target=\"_blank\">Figma</a> prototype, we demonstrated how the extension could make documentation clearer at a high level, reduce wasted developer hours, and make onboarding easier. This project won the JetBrains track at HackDelft 2024.",
+        codeLink: "",
+        resourceLink: "assets/misc/Presentation_HackDelft_2024.pdf",
+        resourceLabel: "Presentation",
+        year: "2023-2024"
+    }, 
+    {
+        title: "Tetris robot",
+        imgSrc: "images/projects/tetris/tetris-robot.jpeg",
+        description: "As part of the Robotics minor at TU Delft, I worked in a multidisciplinary team to design and build a 7-DOF robotic arm capable of stacking parcels into an airplane container. I focused on the software side, developing package detection using a camera and a <a href=\"https://en.wikipedia.org/wiki/Lidar\" target=\"_blank\">LiDAR</a>, and integrating these into <a href=\"https://www.ros.org/\" target=\"_blank\">ROS</a> for localization. I also contributed to testing and motion control, learning the basics of robotic middleware and real-time sensor fusion. The project was done in collaboration with <a href=\"https://www.vanderlande.com/\" target=\"_blank\">Vanderlande</a> and involved creating a full prototype robot from scratch, covering mechanics, electronics, and intelligent software.",
+        codeLink: "",
+        resourceLink: "assets/misc/Robotics_Handover_Document.pdf",
+        resourceLabel: "Report",
+        year: "2021-2022"
     },
     {
         title: "Participation at HackDelft 2022",
@@ -117,7 +118,7 @@ const journey = [
     },
     {
         title: "Software Development Engineer Intern @ Amazon",
-        imgSrc: "https://media.licdn.com/dms/image/v2/C560BAQHTvZwCx4p2Qg/company-logo_100_100/company-logo_100_100/0/1630640869849/amazon_logo?e=1741824000&v=beta&t=5aw62siVrTNOiHjKS7I2MfjVCeVfmOybcJXfKKTcvoI",
+        imgSrc: "images/work/Amazon_logo_square.png", 
         description: "Worked as an SDE Intern in the AI Platforms department of AWS, more specifically in the <a href=\"https://aws.amazon.com/sagemaker-ai/autopilot\" target=\"_blank\">SageMaker Autopilot</a> team, which streamlines the process of building, training, and deploying machine learning models by automating tasks within a machine learning pipeline. I was responsible with conducting a performance benchmarking and testing for an <a href=\"https://aws.amazon.com/lambda\" target=\"_blank\">AWS Lambda</a>-based microservice, analyzing and documenting key metrics, including cold start and warm start latencies, as part of an evaluation to replace the existing solution. This is also the first moment I learned how to use <a href=\"https://www.docker.com/\" target=\"_blank\">Docker</a>. Utilized scripts to automate request generation and data collection, providing detailed reports on Lambda\’s performance impact on the service. Acquired hands-on experience facilitating Scrum ceremonies within a real-world team setting, actively contributing to sprint planning and retrospectives. Took on the role of <a href=\"https://www.scrum.org/resources/what-is-a-scrum-master?utm_source=google&utm_medium=adwords&utm_id=psmii&adgroup={groupid}&gad_source=1&gclid=Cj0KCQiAx9q6BhCDARIsACwUxu5fZXRr_FwwNchCiK7lspOHFDUqwXj9UN5cVTVwonlWXGR2Y7nlN3kaAvz7EALw_wcB\" target=\"_blank\">Scrum Master</a> for a session, guiding the team through agenda items and fostering effective communication to enhance sprint outcomes. Had a wonderful experience thanks to my mentor <a href=\"https://www.linkedin.com/in/nika-vidal-amor\" target=\"_blank\">Nika Vidal Amor</a>, my research mentor <a href=\"https://www.linkedin.com/in/iaroslavshcherbatyi/\" target=\"_blank\">Iaroslav Shcherbatyi</a> and manager <a href=\"https://www.linkedin.com/in/miroslavmiladinovic/\" target=\"_blank\">Miroslav Miladinović</a>.",
         codeLink: "",
         year: "2020-2021"
@@ -188,20 +189,12 @@ const journey = [
         codeLink: "",
         year: "2018-2019"
     },
-    // {
-    //     title: "Computer Science Olympiad 2018",
-    //     imgSrc: "images/projects/folder/icon.jpg",
-    //     description: "11th-12th grade problems, participated in the national olympiad",
-    //     codeLink: "",
-    //     resourceLink: null,
-    //     year: "2017-2018"
-    // },
     {
         title: "Computer Science Olympiad 2017",
         imgSrc: "images/projects/cs-olympiad/bronze-medal.png",
         description: "Continued attending the <a href=\"https://www.nouanepasa.ro/hai-la-olimpiada\" target=\"_blank\">'Hai la olimpiada'</a> training programme, after passing all the entry contests. Started competing in <a href=\"https://codeforces.com/profile/anisca22\" target=\"_blank\">Codeforces</a>. This year I started working on problems involving different data structures (stacks, queues), and more fun problems like 'Evaluare de expresii' using the Reverse Polish notation, and problems involving matrix traversal: using Breadth First Search (BFS), Depth First Search (DFS) and Flood Fill. <br> Participated in the Computer Science Olympiad, got <b>3rd place</b> at the <a href=\"https://www.algopedia.ro/oji2015/2017-10.html\" target=\"_blank\">county stage (OJI) in Cluj</a>, which meant I qualified to the National CS Olympiad (ONI) in Romania, where I received a <b>bronze medal</b>. From OJI I also qualified and participated in the 'Grigore Moisil' regional contest.",
         codeLink: "",
-        resourceLink: null,   
+        resourceLink: null,
         year: "2016-2017"
     },
     {
@@ -225,10 +218,10 @@ const journey = [
         imgSrc: "images/projects/cs-olympiad/hai-la-olimpiada-square.png",
         description: "Attended 'Hai la olimpiada', a weekly training programme for the olympiad with around 30 other students. We mainly solved problems from <a href=\"https://www.infoarena.ro/\" target=\"_blank\">Infoarena</a>. It was everyone's first year coding so we solved very simple problems to get used to C++, loops, recursion, matrices, and basic greedy & dynamic programming problems. One problem that I distinctevely remember solving was <a href=\"https://www.infoarena.ro/job_detail/1581717\" target=\"_blank\">`Spirala`</a> (problem text <a href=\"https://www.infoarena.ro/problema/spirala\" target=\"_blank\">here</a>), because it was one of the first problems which felt intuitive for me to solve. Participated in the Local Stage of the CS Olympiad in Cluj-Napoca - barely got any points (massive fail), I wrote the wrong name for the input file haha. Was selected for a more restricted learning group with 4 other students organized by <a href=\"https://www.facebook.com/learnhouse/\" target=\"_blank\">Learnhouse</a>, taught by <a href=\"https://www.linkedin.com/in/petru-wellcode\" target=\"_blank\">Petru Trimbitas</a>.",
         codeLink: "",
-        resourceLink: null, 
+        resourceLink: null,
         year: "2015-2016"
     },
-    
+
 ];
 
 export { journey };
